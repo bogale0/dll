b1ab230d48125a604c3d46e1ece647d0fe17681e
bogale0 <138402220+bogale0@users.noreply.github.com>
2024-11-21 23:21:01 +0900

    first

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,35 @@
+﻿namespace ClassLibrary1
+{
+    public class Point
+    {
+        public double x, y;
+        public Point(double x, double y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+        public void Rotate(double angle)
+        {
+            double Y = y * Math.Cos(angle) + x * Math.Sin(angle);
+            x = x * Math.Cos(angle) - y * Math.Sin(angle);
+            y = Y;
+        }
+        public void Resize(double coeff)
+        {
+            x *= coeff;
+            y *= coeff;
+        }
+        public double Length()
+        {
+            return Math.Sqrt(x * x + y * y);
+        }
+        public double Angle()
+        {
+            return Math.Atan(y / x);
+        }
+        static public Point operator +(Point a, Point b)
+        {
+            return new Point(a.x + b.x, a.y + b.y);
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,13 @@
+﻿using ClassLibrary1;
+Console.WriteLine("Введите координаты вектора:");
+double read() { return Convert.ToDouble(Console.ReadLine()); }
+Point a = new Point(read(), read());
+Console.WriteLine($"Вектор: ({a.x}; {a.y})");
+Console.WriteLine($"Модуль: {a.Length()}");
+Console.WriteLine($"Угол, °: {a.Angle() * 180 / Math.PI}");
+a.Resize(1.5);
+Console.WriteLine($"Увеличение в 1.5 раза: ({a.x}; {a.y})");
+a = a + new Point(2, 3);
+Console.WriteLine($"Прибавление вектора (2; 3): ({a.x}; {a.y})");
+a.Rotate(Math.PI / 4);
+Console.WriteLine($"Поворот на 45°: ({a.x}; {a.y})");
